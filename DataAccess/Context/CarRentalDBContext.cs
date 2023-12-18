bcfc4e302bfb357bacdbede49f6a7356ff943578
burakgunce <brk.gnc9@hotmail.com>
2023-12-18 10:02:39 +0300

    layers added

---
+++ DataAccess/Context/CarRentalDBContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Context
+{
+    public class CarRentalDBContext : DbContext
+    {
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
